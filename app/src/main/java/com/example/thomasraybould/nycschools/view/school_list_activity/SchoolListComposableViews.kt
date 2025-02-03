6718aa91fa0e2d216ac8282c8f95f0fb4d261a4d
TomRaybould <Thomaseraybould@gmail.com>
2025-02-03 18:54:19 -0500

    Jetpack compose preview working

---
+++ app/src/main/java/com/example/thomasraybould/nycschools/view/school_list_activity/SchoolListComposableViews.kt
@@ -0,0 +1,15 @@
+package com.example.thomasraybould.nycschools.view.school_list_activity
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun Hello(){
+    Column {
+        Text("Testing")
+        Text("working")
+    }
+}
