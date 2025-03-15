f2872f653926f908fadb2991ae865aaf57cb0101
TomRaybould <Thomaseraybould@gmail.com>
2025-03-15 16:05:07 -0400

    Styling added

--- app/src/main/java/com/example/thomasraybould/nycschools/features/selector_activity/SelectorView.kt
+++ app/src/main/java/com/example/thomasraybould/nycschools/features/selector_activity/SelectorView.kt
@@ -1,12 +1,21 @@
 package com.example.thomasraybould.nycschools.features.selector_activity
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.thomasraybould.nycschools.R
 
 @Preview
@@ -16,7 +25,13 @@ private fun SelectorViewPreview() {
         SelectableItem(R.string.rx_constraint_layout, {}),
         SelectableItem(R.string.rx_compose, {}),
     )
-    SelectorViewScreen(SelectorActivityUiModel(selectableItems))
+    Surface(
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        SelectorViewScreen(SelectorActivityUiModel(selectableItems))
+    }
 }
 
 @Composable
@@ -26,17 +41,33 @@ fun SelectorViewScreen(selectorActivityUiModel: SelectorActivityUiModel) {
 
 @Composable
 fun SelectorViewList(selectorActivityUiModel: SelectorActivityUiModel) {
-    LazyColumn {
-        items(selectorActivityUiModel.selectableItems) { item ->
-            SelectableItem(item)
+    Column {
+        Text(
+            modifier = Modifier.padding(4.dp),
+            style = MaterialTheme.typography.titleLarge,
+            text = (stringResource(R.string.selector_activity_title))
+        )
+        Text(
+            modifier = Modifier.padding(4.dp),
+            style = MaterialTheme.typography.bodyMedium,
+            text = (stringResource(R.string.selector_activity_subtitle)))
+        LazyColumn {
+            items(selectorActivityUiModel.selectableItems) { item ->
+                SelectableItem(item)
+            }
         }
     }
 }
 
 @Composable
 fun SelectableItem(selectableItem: SelectableItem) {
-    Button(selectableItem.onItemSelected) {
-        Text(stringResource(selectableItem.activityName))
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = selectableItem.onItemSelected
+        ) {
+            Text(stringResource(selectableItem.activityName))
+        }
     }
 }
 
